dab942ee6c2a853aa3dedfb2f667cfbfe7a48198
Sam Carton <sam.carton@komo.tech>
2024-03-28 11:45:01 +0800

    add custom doc

---
+++ src/pages/_document.tsx
@@ -0,0 +1,32 @@
+
+import Document, {
+  Head,
+  Html,
+  Main,
+  NextScript
+} from 'next/document';
+import React from 'react';
+
+class CustomDocument extends Document {
+
+  render() {
+    return (
+      <Html lang="en">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default CustomDocument;
